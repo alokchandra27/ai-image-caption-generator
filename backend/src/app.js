1b817feda40aa227e1f40acb13f462b8533797d7
alok-404 <alokchandra2621@gmail.com>
2026-07-14 11:15:03 +0530

    Worked on some error

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,14 +9,14 @@ const cors = require("cors")
 const corsOptions = {
   origin: "https://ai-image-caption-generator-phi.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Allow cookies to be sent
+  credentials: true, // Allow cookies to be sent to frontend browser 
 };
 
 const app = express();
 connectToDB()
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors(corsOptions)) // Use the CORS middleware 
+app.use(cors(corsOptions)) // Use the CORS as a middleware 
 
 
 app.use("/api/auth",authRoutes)
